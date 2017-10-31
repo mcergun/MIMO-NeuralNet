2e6898cd7ba46557d404b09d81215d82d89e805d
Mert Can Ergün <mcergun@windowslive.com>
2017-10-31 23:56:18 +0300

    forward propagation seems to work but backwards propagation doesnt work for calculating deltas and i am too tired to debug it further

--- test.m
+++ test.m
@@ -10,23 +10,27 @@ clc
 % % 2 output neurons
 
 output_layer.count = 2;
-output_layer.bias = -0.5;
 output_layer.desired = zeros(output_layer.count, 1);
-hidden_layers(2) = generate_layer(output_layer, 4);
-hidden_layers(1) = generate_layer(hidden_layers(2), 6);
-input_layer = generate_layer(hidden_layers(1), 2);
+hidden_layer = generate_layer(output_layer, 2);
+input_layer = generate_layer(hidden_layer, 2);
 
-input_layer.values = [1; 1];
+% enter data from https://mattmazur.com/2015/03/17/a-step-by-step-backpropagation-example/
+% for validation
 
-hidden_layers(1) = propagate_forward(input_layer, hidden_layers(1));
-hidden_layers(2) = propagate_forward(hidden_layers(1), hidden_layers(2));
-output_layer = propagate_forward(hidden_layers(2), output_layer);
+input_layer.weights = [0.15 0.20; 0.25 0.30];
+input_layer.bias = 0.35;
+input_layer.values = [0.05; 0.1];
+
+hidden_layer.weights = [0.40 0.45; 0.50 0.55];
+hidden_layer.bias = 0.60;
+output_layer.desired = [0.01; 0.99];
+
+hidden_layer = propagate_forward(input_layer, hidden_layer);
+output_layer = propagate_forward(hidden_layer, output_layer);
 
 output_layer = propagate_backward(output_layer);
-hidden_layers(2) = propagate_backward(hidden_layers(2), output_layer);
+hidden_layer = propagate_backward(hidden_layer, output_layer);
+input_layer = propagate_backward(input_layer, hidden_layer);
 
-hidden_layers(1).values
-hidden_layers(2).values
-output_layer.values
-
-input_layer.count = 2;
+hidden_layer.weight_deltas
+input_layer.weight_deltas
